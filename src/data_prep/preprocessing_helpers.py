6b6901eb1ab3f80a2d7d0de4062167e1f272996b
jhonsen <jhonsky@gmail.com>
2020-12-17 19:29:05 -0800

    Revise argparse main

--- src/data_prep/preprocessing_helpers.py
+++ src/data_prep/preprocessing_helpers.py
@@ -1,17 +1,9 @@
 import os
-import sys
 import argparse
 import pandas as pd
 
-data_directory = "../../data/"
-default_path = "data/raw/all-the-news-2-1.csv"
-
-def load_first_subset(filepath, rows):
-    '''Load the raw data file using the first n rows'''
-    dataset = pd.read_csv(filepath, nrows=rows,
-                 usecols = ["date","author","title","publication","section","url", "article"],
-                 parse_dates=['date'])
-    return dataset
+data_directory = "data/"
+raw_filepath = "data/raw/all-the-news-2-1.csv"
     
 def remove_empty_articles(dataset):
     '''Remove empty article columns'''
@@ -21,29 +13,34 @@ def impute_nans(dataset):
     '''Impute string nans of author and section with "unknown" '''
     dataset.loc[dataset.author.isna(), 'author'] = 'unknown'
     dataset.loc[dataset.section.isna(), 'section'] = 'unknown'
+    return dataset
 
-def save_to_parquet(dataset, filename= "subset_1"):
+def save_to_parquet(dataset, filename= "subset_first_15000"):
     '''Save first subset as interim file'''
     filepath = os.path.join(data_directory, 'interim', filename)
     dataset.to_parquet(f'{filepath}.gzip', compression='gzip')
+    print(f'>>>>> Post processing,  {dataset.shape[0]} rows are stored as {filepath}.gzip')
 
-def main(filepath=default_path, rows=150000):
-    df = load_first_subset(filepath)
-    remove_empty_articles(df)
-    impute_nans(df)
-    save_to_parquet(df)
-    print("first subset of data is created!")
+def load_first_subset(filepath, rows):
+    '''Load the first n rows of raw data'''
+    dataset = pd.read_csv(filepath, nrows=rows,
+                 usecols = ["date","author","title","publication","section","url", "article"],
+                 parse_dates=['date'])
+    dataset = remove_empty_articles(dataset)
+    dataset = impute_nans(dataset)    
+    return dataset
+
+def main(filepath=raw_filepath, rows=15000):
+    print(f'>>>>> Loading the first {rows} rows from {raw_filepath}')
+    df = load_first_subset(filepath, rows)
+    save_to_parquet(df, f'subset_first_{rows}')
     
 if __name__=="__main__":
     my_parser = argparse.ArgumentParser(description='Reference the data folder')
-    my_parser.add_argument('Path', metavar='path', type=str,
-                           help='the path to data folder')
-    my_parser.add_argument('Rows', metavar='rows', type=str,
-                           help='the number of rows to load')
+    my_parser.add_argument('Path', metavar='path', type=str, help='the path to data folder')
+    my_parser.add_argument('Rows', type=int, help='the number of rows to load')
     args = my_parser.parse_args()
-    filepath = args.Path
-    n_rows = args.Rows
-
+    filepath, n_rows = args.Path, args.Rows
     main(filepath, n_rows)
     
     
