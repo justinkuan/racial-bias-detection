8a9ccf7c9f865d756ae5d5842baef875ebfe96bf
jhonsen <jhonsky@gmail.com>
2020-12-18 07:52:13 -0800

    put more templates in test_preprocessing_helper

--- tests/data_prep/test_preprocessing_helper.py
+++ tests/data_prep/test_preprocessing_helper.py
@@ -1,24 +1,52 @@
-import os
-import pytest
-
-try:
-    from src.data_prep import preprocessing_helpers
-except ModuleNotFoundError:
-    os.chdir(os.path.join(os.getcwd(), '../../'))
-    print(f'Warning: Changing directory to run test files from {os.getcwd()} directory.')
-    
-from src.data_prep.preprocessing_helpers import convert_os_filepath
+'''TESTs for preprocessing_helper functions'''
 
 class TestProperInput(object):
     
+    '''Test convert_os_filepath'''
+    from src.data_prep.preprocessing_helpers import convert_os_filepath
+
     def test_convert_os_filepath_with_proper_string_input(self):
         assert convert_os_filepath('.\\Users\Documents') == './Users/Documents' 
         assert convert_os_filepath('.\\') == './'
 
+        assert not convert_os_filepath('./Users/Documents') == './/Users//Documents'
+        assert not convert_os_filepath('./') == './/'
+
     def test_convert_os_filepath_with_empty_string_input(self):
         assert convert_os_filepath('') == '' 
-        
+        assert not convert_os_filepath('') == '/'
+
     def test_convert_os_filepath_with_empty_input(self):
         with pytest.raises(TypeError) as exception_info:
             convert_os_filepath()
         assert exception_info.match("missing 1 required positional argument")
+    
+    '''Test remove_empty_articles'''
+    from src.data_prep.preprocessing_helpers import remove_empty_articles
+
+    def test_remove_empty_articles_with_dataframe_input(self):
+        pass
+
+    def test_remove_empty_articles_with_non_dataframe_input(self):
+        pass
+
+    '''Test impute_nans'''
+    from src.data_prep.preprocessing_helpers import impute_nans
+
+    def test_impute_nans_with_dataframe_input(self):
+        pass
+
+    def test_impute_nans_with_non_dataframe_input(self):
+        pass
+
+    '''Test load_first_subset'''
+    
+    '''Test save_to_parquet'''
+    
+class TestProperOutput(object):
+    
+    '''Test convert_os_filepath'''
+    from src.data_prep.preprocessing_helpers import convert_os_filepath
+
+    def test_convert_os_filepath_with_proper_output(self):
+        pass
