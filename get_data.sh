cb08680583833167f12fcd0b6a5977cf789a753a
jhonsen <jhonsky@gmail.com>
2020-12-27 16:42:18 -0800

    Add first 15000 rows subset data and labeled data

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Create a subset of news data, default to first ~15000 rows
 python ./src/data_prep/preprocessing_helpers.py -f -r
+# Add labels to subset using topic modeling
+python ./src/date_prep/topic_modeling_helpers.py
